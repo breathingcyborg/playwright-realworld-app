0044043c2840ab0f9a42535ec19e0330f51a5e1f
breathingcyborg <breathingcyborg@gmail.com>
2024-12-11 19:14:06 +0530

    add axios-retry

--- tests/helpers/backend-axios.ts
+++ tests/helpers/backend-axios.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { BACKEND_URL } from "../constants";
+import axiosRetry from 'axios-retry';
 
 export const backendAxios = axios.create({
     baseURL: BACKEND_URL,
-})
+});
+
+axiosRetry(
+    backendAxios,    
+    { 
+        retries: 3, 
+        retryDelay: axiosRetry.exponentialDelay 
+    }
+)
